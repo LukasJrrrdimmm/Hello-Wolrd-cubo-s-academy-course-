eb05d4dcbc8cd31a11b2924673ee5ca23867c7ab
LukasJrrrdimmm <lukasmaximo@gmail.com>
2023-07-20 21:52:12 -0300

    Adição OurScript1.js

--- HelloWolrd.js
+++ HelloWolrd.js
@@ -1,7 +1,9 @@
 console.log("Hello Wolrd!");
 const primeiroNome = "Lukas";
 let X = 1.0;
-for(var i = 0; i < 10; i++){
+for(let i = 0; i < 10; i++){
     X += 0.5;
 }
 console.log(X)
+
+console.log(`Testo Espião Aleatório o valor de x é ${X}`)
---
+++ OurScript1.js
@@ -0,0 +1,10 @@
+const cidade = "Natal";
+const estado = "RN";
+const pais = "Brasil";
+
+console.log(`${cidade}\\${estado} -- ${pais}`);
+
+let populacao =  3000000;
+console.log(`check 1 ${populacao}`)
+populacao = 3050000;
+console.log(`check 2 ${populacao}`)
