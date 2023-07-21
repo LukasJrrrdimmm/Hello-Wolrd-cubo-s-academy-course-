b2057581c3ea9776bf70660a941d288108bbb790
LukasJrrrdimmm <lukasmaximo@gmail.com>
2023-07-20 22:32:34 -0300

    Adicionado Opera.js

---
+++ Opera.js
@@ -0,0 +1,18 @@
+let x = 13, y = 4
+
+let z = 0;
+
+z = x+y
+console.log(`soma entre ${x} e ${y} = ${z}`)
+z = x-y
+console.log(`subtração entre ${x} e ${y} = ${z}`)
+z = x*y
+console.log(`multiplicação entre ${x} e ${y} = ${z}`)
+z = x/y
+console.log(`divisão entre ${x} e ${y} = ${z}`)
+z = x**y
+console.log(`potenciação de ${x} elevado à ${y} = ${z}`)
+z = x**(1/y)
+console.log(`radiciação da raiz ${y}ª de ${x} = ${z}`)
+z = Math.log(x)/Math.log(y)
+console.log(`logaritimo de ${x} na base ${x} = ${z}`)
